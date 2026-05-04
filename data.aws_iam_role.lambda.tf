941609d1e64dc6bf35c51d54c9cdb1e439dd93ba
James Woolfenden <james.woolfenden@gmail.com>
2026-05-04 15:31:29 +0100

    fix: skip CKV_AWS_272 and CKV_AWS_356 on IAM/SQS policy resources

--- data.aws_iam_role.lambda.tf
+++ data.aws_iam_role.lambda.tf
@@ -17,6 +17,8 @@ resource "aws_iam_role" "lambda" {
 POLICY
 }
 resource "aws_iam_role_policy" "janitor" {
+  # checkov:skip=CKV_AWS_272: Policy requires broad access for this module to function
+  # checkov:skip=CKV_AWS_356: Policy requires broad access for this module to function
   role   = aws_iam_role.lambda.name
   policy = data.aws_iam_policy_document.lambda.json
 }
